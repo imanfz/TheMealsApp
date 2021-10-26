84587be0b988a924d40bd22d5644ae10ce664fd2
Iman Faizal <55774481+imanfz@users.noreply.github.com>
2021-10-27 06:19:12 +0700

    API
    adding ApiService and SwiftLint

---
+++ TheMealsApp/Core/Utils/Network/ApiService.swift
@@ -0,0 +1,25 @@
+//
+//  ApiService.swift
+//  TheMealsApp
+//
+//  Created by Iman Faizal on 27/10/21.
+//
+
+import Foundation
+
+struct API {
+  static let baseUrl = "https://www.themealdb.com/api/json/v1/1/"
+}
+
+struct ApiService {
+  static let getCategories: String = "\(API.baseUrl)categories.php"
+  static let getMealsByCategoryName: (String) -> String = { categoryName in
+    return "\(API.baseUrl)filter.php?c=\(categoryName)"
+  }
+  static let getMealsDetail: (Int) -> String = { mealsId in
+    return "\(API.baseUrl)lookup.php?i=\(mealsId)"
+  }
+  static let searchByName: (String) -> String = { query in
+    return "\(API.baseUrl)search.php?s=\(query)"
+  }
+}
